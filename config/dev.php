c78cb6a2ae5cef24510490dbfd4b1d58be8cf596
Haralan Dobrev <hkdobrev@gmail.com>
2014-06-22 23:57:19 +0300

    Enable the Silex Web Profiler in development

--- config/dev.php
+++ config/dev.php
@@ -1,7 +1,7 @@
 <?php
 
 use Silex\Provider\MonologServiceProvider;
-// use Silex\Provider\WebProfilerServiceProvider;
+use Silex\Provider\WebProfilerServiceProvider;
 
 // include the default configuration
 require __DIR__.'/default.php';
@@ -18,6 +18,6 @@ $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/log/silex_dev.log',
 ));
 
-// $app->register(new WebProfilerServiceProvider(), array(
-//     'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
-// ));
+$app->register(new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
+));
