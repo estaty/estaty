039fc753e94b5d815f9428a7591dc7b8a435f227
Haralan Dobrev <hkdobrev@gmail.com>
2014-06-22 23:44:41 +0300

    Enable logging

--- config/dev.php
+++ config/dev.php
@@ -1,6 +1,6 @@
 <?php
 
-// use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\MonologServiceProvider;
 // use Silex\Provider\WebProfilerServiceProvider;
 
 // include the default configuration
@@ -12,9 +12,9 @@ ini_set('display_errors', 1);
 // enable the debug mode
 $app['debug'] = true;
 
-// $app->register(new MonologServiceProvider(), array(
-//     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
-// ));
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/log/silex_dev.log',
+));
 
 // $app->register(new WebProfilerServiceProvider(), array(
 //     'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
