c420c40ddabf327c3154583da08c2b3f93da603e
Haralan Dobrev <hkdobrev@gmail.com>
2014-06-22 22:52:10 +0300

    Register TwigServiceProvider with the app

--- src/app.php
+++ src/app.php
@@ -2,7 +2,7 @@
 
 use Estaty\Application;
 
-// use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 // use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
@@ -11,7 +11,7 @@ $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 // $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
-// $app->register(new TwigServiceProvider());
+$app->register(new TwigServiceProvider());
 // $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
 //     // add custom globals, filters, tags, ...
 
