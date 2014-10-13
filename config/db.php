913c59ee6e8c130b5082e11ee678ec3c21a2f349
Haralan Dobrev <hkdobrev@gmail.com>
2014-10-13 23:04:34 +0300

    Add Doctrine configuration

---
+++ config/db.php
@@ -0,0 +1,22 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+// the connection configuration
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'vagrant',
+    'password' => null,
+    'dbname'   => 'estaty',
+);
+
+$paths = [__DIR__.'/../src/Model'];
+$isDevMode = !empty($app['debug']);
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'db.options' => $app['db.options'],
+]);
+
+$app['orm.meta.config'] = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$app['orm.em'] = EntityManager::create($app['db.options'], $app['orm.meta.config']);
