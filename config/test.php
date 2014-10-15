fe683f430ea3616fd4105942e5a42f1246c718d3
Haralan Dobrev <hkdobrev@gmail.com>
2014-10-16 02:18:27 +0300

    Add a bit of test environment configuration

--- config/test.php
+++ config/test.php
@@ -1,1 +1,13 @@
 <?php
+
+use Silex\Provider\MonologServiceProvider;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$app['twig.options'] = ['debug' => false, 'cache' => false];
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/log/silex_test.log',
+    'monolog.name' => 'estaty.test'
+));
