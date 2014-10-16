1693d2c41bb5754950d883603104e9030e0f1024
Haralan Dobrev <hkdobrev@gmail.com>
2014-10-17 01:45:32 +0300

    Load controllers only if $app is already loaded

--- src/controllers.php
+++ src/controllers.php
@@ -3,6 +3,11 @@
 use Estaty\Application;
 use Estaty\Controller\HomepageController;
 
+if (!isset($app)) {
+    // This file should only be loaded after the Silex app is initialized
+    return;
+}
+
 $app['homepage.controller'] = $app->share(function() {
     return new HomepageController();
 });
