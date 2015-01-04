08ea832b725e012d2fd49d6cf93ea0aae8b89220
Haralan Dobrev <hkdobrev@gmail.com>
2015-01-05 00:41:15 +0200

    Require Monolog bridge and update Monolog

---
+++ config/secret/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+define('FACEBOOK_APP_ID', '');
+define('FACEBOOK_APP_SECRET', '');
+
+define('GOOGLE_OAUTH_CLIENT_ID', '');
+define('GOOGLE_OAUTH_CLIENT_SECRET', '');
+
+define('GITHUB_OAUTH_CLIENT_ID', '');
+define('GITHUB_OAUTH_CLIENT_SECRET', '');
