725fa3148cb9f7704e8d1e1adc3a598c5741c5a9
Haralan Dobrev <hkdobrev@gmail.com>
2015-01-13 21:03:38 +0200

    Format code on multiple lines

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -53,7 +53,10 @@ class UserRepository extends EntityRepository implements UserProviderInterface, 
     public function refreshUser(UserInterface $user)
     {
         if (!$this->supportsClass(get_class($user))) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+            throw new UnsupportedUserException(sprintf(
+                'Instances of "%s" are not supported.',
+                get_class($user)
+            ));
         }
 
         return $this->loadUserByUsername($user->getUsername());
